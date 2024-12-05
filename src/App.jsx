b77a2b0cdc5245facf969b20a6a94431bbbb3545
davidebalice <davide.balice@gmail.com>
2024-12-05 12:09:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AudioPlayer from "./components/AudioPlayer.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
     <>
-        <AudioPlayer />
+      <AudioPlayer />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import classes from "../index.module.css";
+
+function Footer() {
+  return (
+    <div className={classes.footer}>
+      <a href="https://www.davidebalice.dev" target="_blank" rel="noreferrer">
+        www.davidebalice.dev
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
